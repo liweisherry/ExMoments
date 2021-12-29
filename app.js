444274d6ae10ae93752b41f008280f2c04648b9f
liweisherry <yjrsherry@gmail.com>
2021-12-29 18:09:42 -0500

    Add start script

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+if (process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+}
 const express = require('express');
 const mongoose = require('mongoose');
-const Moment = require('./models/moment');
 const ejsMate = require('ejs-mate')
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require('method-override');
-const Review = require('./models/review');
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const moments = require('./routes/moments');
+const reviews = require('./routes/reviews');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const userRoutes = require('./routes/users');
+const MongoStore = require('connect-mongo');
+const db_url = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+// process.env.DB_URL
+// 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(db_url, {
     useNewUrlParser: true,
     // useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    // useFindAndModify: false
 })
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
@@ -26,92 +39,52 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
-// Middleware application
-const validateMoment = (req, res, next) =>{
-    // if(!req.body.moment) throw new ExpressError('Invalid Moment Data', 400);
 
-    const {error} = momentSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg,400)
-    }else {
-        next();
+const secret = process.env.SECRET || 'thisshouldbebettersecret!'
+const store = MongoStore.create({
+    mongoUrl: db_url,
+    secret,
+    touchAfter: 24 * 3600
+})
+store.on("error", function(e){
+    console.log("Session store error")
+})
+const seesionConfig ={
+    store,
+    name:'session',
+    secret,
+    resave:false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 *7,
+        maxAge: 1000 * 60 * 60 * 24 *7
     }
-}
 
-const validateReview =(req, res, next) =>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg,400)
-    }else {
-        next();
-    }
 }
+app.use(session(seesionConfig))
+
+// flash a message for all requests
+app.use(flash())
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next) =>{
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/Moments', async (req, res) => {
-    const Moments = await Moment.find({})
-    res.render('Moments/index', {Moments})
-})
+app.use('/', userRoutes);
+app.use('/Moments', moments);
 
-// Add a new moment
-app.get('/Moments/new', async (req, res) => {
-    res.render('Moments/new');
-})
-
-// Show all Moments
-app.post('/Moments', validateMoment, catchAsync(async (req, res, next) => {
-
-    const moment = new Moment(req.body.moment);
-    await moment.save();
-    res.redirect(`/Moments/${moment._id}`);
-}))
-
-// Show one specific moment
-app.get('/Moments/:id',  catchAsync(async (req, res) => {
-    const moment = await Moment.findById(req.params.id).populate('reviews');
-    res.render('Moments/show', {moment});
-}))
-
-// Edit existing Moments
-app.get('/Moments/:id/edit', catchAsync( async (req, res) => {
-    const moment = await Moment.findById(req.params.id)
-    res.render('Moments/edit', {moment});
-}))
-app.put('/Moments/:id', validateMoment, catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const moment = await Moment.findByIdAndUpdate(id, {...req.body.moment});
-    res.redirect(`/Moments/${moment._id}`);
-}))
-
-// Delete a moment
-app.delete('/Moments/:id',  catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const moment = await Moment.findByIdAndDelete(id);
-    res.redirect(`/Moments`);
-}))
-
-// Add a review under a moment
-app.post('/Moments/:id/reviews', validateReview, catchAsync(async (req,res,next)=>{
-    const moment = await Moment.findById(req.params.id);
-    const review = new Review(req.body.review);
-    moment.reviews.push(review);
-    await review.save();
-    await moment.save();
-    res.redirect(`/Moments/${moment._id}`);
-}
-))
-
-// Delete a review
-app.delete('/Moments/:id/reviews/:reviewId', catchAsync(async(req,res,next)=>{
-    const {id, reviewId} = req.params;
-    await  Moment.findByIdAndUpdate(id, {$pull:{reviews:reviewId}})
-    await  Review.findByIdAndDelete(req.params.reviewId);
-    res.redirect(`/Moments/${id}`);
-}))
+app.use('/Moments/:id/reviews', reviews);
 
 app.all('*', (req, res,next) =>{
     next(new ExpressError('Page Not Found', 404))
@@ -124,7 +97,7 @@ app.use((err, req, res, next) => {
         res.status(statusCode).render('error', {err});
 
 })
-
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
---
+++ routes/moments.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const Moment = require("../models/moment");
+const catchAsync = require("../utils/catchAsync");
+const {momentSchema} = require("../schemas");
+const ExpressError = require("../utils/ExpressError");
+const router = express.Router();
+
+// Middleware application
+const validateMoment = (req, res, next) =>{
+    // if(!req.body.moment) throw new ExpressError('Invalid Moment Data', 400);
+
+    const {error} = momentSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg,400)
+    }else {
+        next();
+    }
+}
+
+router.get('/', async (req, res) => {
+    const Moments = await Moment.find({})
+    res.render('Moments/index', {Moments})
+})
+
+// Add a new moment
+router.get('/new', async (req, res) => {
+    res.render('Moments/new');
+})
+
+// Show all Moments
+router.post('/', validateMoment, catchAsync(async (req, res, next) => {
+
+    const moment = new Moment(req.body.moment);
+    await moment.save();
+    req.flash('success', 'Successfully made a new moment')
+    res.redirect(`/Moments/${moment._id}`);
+}))
+
+// Show one specific moment
+router.get('/:id',  catchAsync(async (req, res) => {
+    const moment = await Moment.findById(req.params.id).populate('reviews');
+    if(!moment){
+        req.flash('error', 'Can not find that moment');
+        return res.redirect('/Moments')
+    }
+    res.render('Moments/show', {moment});
+}))
+
+// Edit existing Moments
+router.get('/:id/edit', catchAsync( async (req, res) => {
+    const moment = await Moment.findById(req.params.id)
+    if(!moment){
+        req.flash('error', 'Can not find that moment');
+        return res.redirect('/Moments')
+    }
+    res.render('Moments/edit', {moment});
+}))
+router.put('/:id', validateMoment, catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const moment = await Moment.findByIdAndUpdate(id, {...req.body.moment});
+    res.redirect(`/Moments/${moment._id}`);
+}))
+
+// Delete a moment
+router.delete('/:id',  catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const moment = await Moment.findByIdAndDelete(id);
+    res.flash('success', 'Successfully delete a moment!')
+    res.redirect(`/Moments`);
+}))
+
+module.exports = router;
